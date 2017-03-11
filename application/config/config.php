f85295105ab7558a7f7a74f3e4207ab108919f08
Hien Le <hienlt0610@gmail.com>
2017-03-11 22:03:28 +0700

    some config

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/doan';
+$config['base_url'] = 'http://115.76.33.100:8080/doan';
 
 /*
 |--------------------------------------------------------------------------
